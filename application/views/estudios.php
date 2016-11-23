1c762f9d3c5991c40fc9f66e506e66d8f003ea29
edinho <edinhosiljr@gmail.com>
2016-11-23 19:40:17 +0000

    leo/pagina de estudio funcionando/pequenas alterações visuais

--- application/views/estudios.php
+++ application/views/estudios.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
+<main class="inicio back-black-tp">
 <section <?= 'id="estudio'.$id_estudio.'"'; ?> class="projeto">
       <div <?= 'style=" background-image: url('.$ds_img.');" alt="imagem-'.$nm_estudio.'"'; ?> class="img1"></div>
       <h2><?=$nm_estudio?> </h2>
